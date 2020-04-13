fab225a3cb8c2447b65dca441764c4e468a858c4
Devone Austin <devone.austin@gmail.com>
2020-04-13 21:46:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(Josh)
   puts "Hi, my name is #{name}."
 end
 
-def introduction(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am leanring to program in #{language}."
 end
